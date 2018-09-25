285344b14224faca816eec25eec797378de65f77
Nattapol Praewpipat <nattapol.praew@gmail.com>
2018-09-25 23:52:16 +0700

    back up file

--- src/pages/report-checkup/report-checkup.ts
+++ src/pages/report-checkup/report-checkup.ts
@@ -22,7 +22,11 @@ export class ReportCheckupPage {
   testSelect:any
   constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
     this.report = navParams.get('report');
-    this.testSelect = {showDetails:false,icon:'arrow-dropup'}
+    this.testSelect = {
+      title: 'Title ',
+      details: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      showDetails:false,
+      icon:'arrow-dropup'}
     for(let i = 0; i < 10; i++ ){
       this.data.push({
           title: 'Title '+i,
